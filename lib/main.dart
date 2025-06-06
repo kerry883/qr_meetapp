30ef693a6f83cadebdd9e54b519bcb14eea2175c
kerry883 <okerry883@gmail.com>
2025-06-06 14:49:51 +0300

    Color and Debug option set False

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(213, 43, 166, 211)),
       ),
-      home: const MyHomePage(title: 'QR MeetApp'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'QRMeetApp'),
     );
   }
 }
